9a2410c81d346eb73f1765c134b201f689939b43
Fjor Robles <126599236+Fjorrobles@users.noreply.github.com>
2026-03-05 08:32:17 -0700

    Menu File
    This is where the user interacts with the program

---
+++ ProjectMenu.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string closetName;
+    string addToCloset;
+    cout<<"Welcome to the outfit selector program!"<<endl;
+    cout<<"What is your name: "<<endl;
+    cin>>closetName;
+    cout<<"Would you like to add things to your closet "<<closetName<<" ?: yes or no"<<endl;
+    cin>>addToCloset;
+    if (addToCloset == "yes"){
+        
+    }
+    
+
+}
