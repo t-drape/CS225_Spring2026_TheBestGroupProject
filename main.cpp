8c7a9a01305802098e2193839c521e20de760c7a
t-drape <thebatreview1@gmail.com>
2026-03-08 12:46:08 -0700

    Create convertHSLtoRGB

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 class HSLColor {
     private:
-        int hue; // Measured in degrees
-        int saturation; // Measured in percentages
-        int lightness; // Measured in percentages
+        double hue; // Measured in degrees
+        double saturation; // Measured in percentages
+        double lightness; // Measured in percentages
     public:
 
         int getHue() {return hue;}
+        double getLightness() {return lightness;}
+        double getSaturation() {return saturation;}
 
-        HSLColor(int hue = 40, int saturation = 35, int lightness = 75) {
+        HSLColor(double hue = 40, double saturation = 35, double lightness = 75) {
             if (hue > 359) {
                 hue -= 360;
             }
@@ -30,10 +32,25 @@ class HSLColor {
 };
 
 class RGBColor {
-    public:
+    private:
         int redValue;
         int greenValue;
         int blueValue;
+    public:
+        RGBColor(int r, int g, int b) {
+            redValue = r;
+            greenValue = g;
+            blueValue = b;
+        }
+
+        friend ostream& operator<<(ostream& os, RGBColor& color) {
+            os << "Red: " << color.redValue;
+            os << " Green: " << color.greenValue;
+            os << " Blue: " << color.blueValue;
+            os << endl;
+            return os;
+        }
+
 };
 
 class Node {
@@ -94,7 +111,9 @@ bool inRange(int value, int lowerBound, int upperBound) {
 
 int* tetradicPaletteGenerator(int hue) {
     /*
-    Reference: https://www.digitalocean.com/community/tutorials/return-array-in-c-plus-plus-function
+    References:
+    color function: https://customstickers.com/community/blog/how-to-calculate-complementary-triadic-and-tetradic-colors-from-a-hex-code?srsltid=AfmBOoqEuD3xMxsxkL3ULkV0TfeZKsFD4GemfKF8-prqylBgYfmWh-Bo
+    return array: https://www.digitalocean.com/community/tutorials/return-array-in-c-plus-plus-function
     */
 
     static int colorPaletteHues[4];
@@ -148,6 +167,64 @@ Node* matchingClothes(HSLColor* closet, int* hues) {
 
 }
 
+RGBColor* convertHSLtoRGB(HSLColor& color) {
+    /*
+    Reference: https://www.baeldung.com/cs/convert-color-hsl-rgb
+    */
+
+    // Chroma equation: (1 - |2L - 1|) x S
+    double chromaP2 = (2 * color.getLightness() - 1);
+    if (chromaP2 < 0) {
+        chromaP2 *= -1;
+    }
+    double chroma = (1 - chromaP2) * color.getSaturation();
+    cout << "Chroma: " << chroma << endl;
+
+    double hue_derivative = color.getHue() / 60.0;
+    cout << "Hue Derivative: " << hue_derivative << endl;
+
+    // X = C * (1 - |(H' % 2) - 1|})
+    double xP3 = (fmod(hue_derivative, 2.0)) - 1;
+    if (xP3 < 0) {
+        xP3 *= -1;
+    }
+    double x = chroma * (1 - xP3);
+    cout << "X: " << x << endl;
+
+    double r,g,b = 0;
+    if (hue_derivative <= 1) {
+        r = chroma;
+        g = x;
+    } else if (hue_derivative <= 2) {
+        r = x;
+        g = chroma;
+    } else if (hue_derivative <= 3) {
+        g = chroma;
+        b = x;
+    } else if (hue_derivative <= 4) {
+        g = x;
+        b = chroma;
+    } else if (hue_derivative <= 5) {
+        r = x;
+        b = chroma;
+    } else if (hue_derivative <= 6) {
+        r = chroma;
+        b = x;
+    }
+
+    double m = color.getLightness() - (chroma / 2);
+    cout << "M: " << m << endl;
+
+    cout << "R1, G1, B1: " << r << ", " << g<< ", " << b << endl;
+    r += m;
+    g += m;
+    b += m;
+    cout << "R1+m, G1+m, B1+m: " << r << ", " << g<< ", " << b << endl;
+
+    RGBColor* nc = new RGBColor(r * 255, g * 255, b * 255);
+    return nc;
+}
+
 
 int main() {
     /*
@@ -192,33 +269,12 @@ int main() {
     head.head = NULL;
 
     head.head = matchingClothes(closetColors, colorPaletteHues);
-    // do {
-    //     /*
-    //     For clothes in partitioned closet,
-    //     If hue is within range,
-    //     Add to matchingColor Linked list of Clothes objects
-    //     */
-    //     for(int i = 0; i < 10; i++) {
-    //         for (int j = 0; j < 4; j++) {
-    //             /*
-    //             Originally, I expanded both sides. This led to too many colors included.
-    //             So, the lower bound equals the original value, the upper bound equals the computed value.
-    //             */
-    //             int upperBound = colorPaletteHues[j] + (36 * loops);
-    //             if (upperBound > 359) {
-    //                 upperBound -= 360;
-    //             }
-    //             if (inRange(closetColors[i].getHue(), colorPaletteHues[j], upperBound)) {
-    //                 // Change to a Clothes Object once we have full project developed
-    //                 head.addUnique(closetColors[i].getHue());
-    //                 match = true;
-    //             }
-    //         }
-    //     }
-    //     loops++;
-    // } while (match == false);
 
     cout << head;
+
+    HSLColor test(136, .54, .43);
+    RGBColor* ptr = convertHSLtoRGB(test);
+    cout << *ptr;
 
     return 0;    
 }
