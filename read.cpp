034a7b7b49eb1abd18d6b6c75280ae1804957074
t-drape <thebatreview1@gmail.com>
2026-03-08 14:05:23 -0700

    Add python program call to read.cpp

--- read.cpp
+++ read.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+
+/*
+Sources of Help:
+Grok for seeing how to implement python in a C++ program
+*/
 
 using namespace std;
 
 int main() {
+    std::system("python3 color_picker.py");
+
     ifstream fin("chosen_color.txt");
     string r;
     fin >> r;
