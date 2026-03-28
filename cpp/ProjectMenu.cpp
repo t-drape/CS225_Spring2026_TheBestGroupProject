0dd097301d74b5326d593028c8511edb36c0127a
t-drape <thebatreview1@gmail.com>
2026-03-28 13:06:01 -0700

    Fix bounds error in matchingClothes, add swap function

--- cpp/ProjectMenu.cpp
+++ cpp/ProjectMenu.cpp
@@ -5,11 +5,25 @@
 #include "./../hpp/clothes.hpp"
 #include "./../hpp/colorDisplay.hpp"
 
-enum {SHORTS, SHIRTS};
-enum {COLD, WARM};
-enum {NOT_COLORFUL, COLORFUL};
 
 using namespace std;
+
+// Function Definition from read.hpp
+vector<int> getColors() {
+    vector<int> colors;
+    std::system("python3 ./../colorPicker/color_picker.py");
+    ifstream fin("./../colorPicker/chosen_color.txt");
+    string r;
+    fin >> r;
+    string g;
+    fin >> g;
+    string b;
+    fin >> b;
+    colors.push_back(stoi(r));
+    colors.push_back(stoi(g));
+    colors.push_back(stoi(b));
+    return colors;
+}
 
 void print_colored_rgb(int r, int g, int b, const std::string& text) {
     // Set foreground color using RGB values
@@ -43,6 +57,135 @@ string pants() {
     return content;
 }
 
+// Function Definition from colors.hpp
+RGBColor* convertHSLtoRGB(HSLColor& color) {
+    /*
+    Reference: https://www.baeldung.com/cs/convert-color-hsl-rgb
+    */
+
+    // Chroma equation: (1 - |2L - 1|) x S
+    double chromaP2 = (2 * color.getLightness() - 1);
+    if (chromaP2 < 0) {
+        chromaP2 *= -1.0;
+    }
+    double chroma = (1 - chromaP2) * color.getSaturation();
+    // cout << "Chroma: " << chroma << endl;
+
+    double hue_derivative = color.getHue() / 60.0;
+    // cout << "Hue Derivative: " << hue_derivative << endl;
+
+    // X = C * (1 - |(H' % 2) - 1|})
+    double xP3 = (fmod(hue_derivative, 2.0)) - 1;
+    if (xP3 < 0) {
+        xP3 *= -1.0;
+    }
+    double x = chroma * (1 - xP3);
+    // cout << "X: " << x << endl;
+
+    double r = 0;
+    double g = 0;
+    double b = 0;
+    if (hue_derivative <= 1) {
+        r = chroma;
+        g = x;
+    } else if (hue_derivative <= 2) {
+        r = x;
+        g = chroma;
+    } else if (hue_derivative <= 3) {
+        g = chroma;
+        b = x;
+    } else if (hue_derivative <= 4) {
+        g = x;
+        b = chroma;
+    } else if (hue_derivative <= 5) {
+        r = x;
+        b = chroma;
+    } else if (hue_derivative <= 6) {
+        r = chroma;
+        b = x;
+    }
+
+    double m = color.getLightness() - (chroma / 2.0);
+    // cout << "M: " << m << endl;
+
+    // cout << "R1, G1, B1: " << r << ", " << g<< ", " << b << endl;
+    r += m;
+    g += m;
+    b += m;
+
+    // cout << "R1+m, G1+m, B1+m: " << r << ", " << g<< ", " << b << endl;
+    RGBColor* nc = new RGBColor(r * 255, g * 255, b * 255);
+    return nc;
+}
+
+HSLColor* convertRGBtoHSL(RGBColor& color) {
+    /*
+    References: https://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
+    */
+
+    double red = color.getRedValue();
+    double green = color.getGreenValue();
+    double blue = color.getBlueValue();
+
+    red /= 255;
+    green /= 255;
+    blue /= 255;
+
+    double max;
+    double min;
+
+    if (red >= green && red >= blue) {
+        max = red;
+    } else if (green >= red && green >= blue) {
+        max = green;
+    } else if (blue >= red && blue >= green) {
+        max = blue;
+    }
+
+    if (red <= green && red <= blue) {
+        min = red;
+    } else if (green <= red && green <= blue) {
+        min = green;
+    } else if (blue <= red && blue <= green) {
+        min = blue;
+    }
+
+    double l = (min + max) / 2.0;
+
+    double sat;
+    double hue;
+
+    if (max == min) {
+        sat = 0;
+        hue = 0;
+    } else if (l <= 0.5) {
+        sat = (max-min)/(max+min);
+    } else if (l > 0.5) {
+        sat = (max-min)/(2.0-max-min);
+    }
+
+    double h;
+
+    if (red == max) {
+        h = (green - blue)/(max-min);
+    } else if (green == max) {
+        h = (2.0) + (blue-red)/(max-min);
+    } else if (blue == max) {
+        h = (4.0) + (red-green)/(max-min);
+    }
+
+    h *= 60;
+
+    if (h < 0) {
+        h += 360;
+    }
+
+    HSLColor* nc = new HSLColor(h, sat, l);
+    return nc;
+}
+
+
+// Function Definition from clothes.hpp
 vector<Clothes> createCloset(const string filePath) {
     /*
     Purpose: Create a vector representation of the Clothes objects in the user's closet file
@@ -104,7 +247,7 @@ vector<Clothes> createCloset(const string filePath) {
 }
 
 // This refines the closet based on user input to create a more realistic clothes matching application
-vector<Clothes> findShirts(const string filePath, int w, int colorful, string chosenGraphic, int chosenType=SHIRTS) {
+vector<Clothes> findShirts(const string filePath, int w, int colorful, string chosenGraphic, int chosenType) {
     /*
     Purpose: Create a vector representation of the Clothes objects in the user's closet file
     References: C++ documentation on std::stod function
